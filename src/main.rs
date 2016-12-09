2bab80ff9bd84bdc2d5e50c647e9fdd3d9975cb1
Samuel Gélineau <gelisam+github@gmail.com>
2016-12-09 18:18:02 -0500

    refactor to top-level functions

--- src/main.rs
+++ src/main.rs
@@ -11,42 +11,55 @@ use carboxyl_time::*;
 use glutin_window::GlutinWindow as Window;
 use opengl_graphics::{ GlGraphics, OpenGL };
 use piston::event_loop::*;
-use piston::input::*;
 use piston::window::WindowSettings;
 
-pub struct App {
-    gl: GlGraphics, // OpenGL drawing backend.
-    rotation: Signal<f64>   // Rotation for the square.
+
+#[derive(Clone)]
+struct Context {
+  square_rotation: f64,
 }
 
-impl App {
-    fn render(&mut self, args: &RenderArgs) {
-        use graphics::*;
 
-        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
-        const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+fn frp_network() -> Signal<Context> {
+  let seconds: Signal<f64> = {
+    let time: Signal<time::Tm> = now();
+    let t0 = time.sample();
+    lift!(move |t| (t - t0).num_milliseconds() as f64 / 1000.0, &time)
+  };
 
-        let square = rectangle::square(0.0, 0.0, 50.0);
-        let rotation = self.rotation.sample();
-        let (x, y) = ((args.width / 2) as f64,
-                      (args.height / 2) as f64);
+  let square_rotation = lift!(|t| 2.0 * t, &seconds); // Rotate 2 radians per second.
 
-        self.gl.draw(args.viewport(), |c, gl| {
-            // Clear the screen.
-            clear(GREEN, gl);
+  lift!(|r|
+    Context {
+      square_rotation: r,
+    },
+    &square_rotation
+  )
+}
 
-            let transform = c.transform.trans(x, y)
-                                       .rot_rad(rotation)
-                                       .trans(-25.0, -25.0);
 
-            // Draw a box rotating around the middle of the screen.
-            rectangle(RED, square, transform, gl);
-        });
-    }
+fn render(gl: &mut GlGraphics, args: &piston::input::RenderArgs, context: &Context) {
+  use graphics::*;
 
-    fn update(&mut self, args: &UpdateArgs) {
-        //self.dt.send(args.dt);
-    }
+  const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
+  const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+
+  let square = rectangle::square(0.0, 0.0, 50.0);
+  let rotation = context.square_rotation;
+  let (x, y) = ((args.width / 2) as f64,
+                (args.height / 2) as f64);
+
+  gl.draw(args.viewport(), |c, gl| {
+    // Clear the screen.
+    clear(GREEN, gl);
+
+    let transform = c.transform.trans(x, y)
+                               .rot_rad(rotation)
+                               .trans(-25.0, -25.0);
+
+    // Draw a box rotating around the middle of the screen.
+    rectangle(RED, square, transform, gl);
+  });
 }
 
 fn main() {
@@ -55,34 +68,25 @@ fn main() {
 
     // Create an Glutin window.
     let mut window: Window = WindowSettings::new(
-            "spinning-square",
-            [200, 200]
-        )
-        .opengl(opengl)
-        .exit_on_esc(true)
-        .build()
-        .unwrap();
+        "spinning-square",
+        [200, 200]
+      )
+      .opengl(opengl)
+      .exit_on_esc(true)
+      .build()
+      .unwrap();
+    let mut gl = GlGraphics::new(opengl);
 
-    // Create a new game and run it.
-    let seconds: Signal<f64> = {
-      let time: Signal<time::Tm> = now();
-      let t0 = time.sample();
-      lift!(move |t| (t - t0).num_milliseconds() as f64 / 1000.0, &time)
-    };
-
-    let rotation = lift!(|t| 2.0 * t, &seconds); // Rotate 2 radians per second.
-    let mut app = App {
-        gl: GlGraphics::new(opengl),
-        rotation: rotation
-    };
+    let context = frp_network();
 
     let mut events = window.events();
     while let Some(e) = events.next(&mut window) {
-        match e {
-          Event::Render(r) => app.render(&r),
-          Event::Update(u) => app.update(&u),
-          _ => ()
-        }
+      use piston::input::Event::{ Render };
+
+      match e {
+        Render(args) => render(&mut gl, &args, &context.sample()),
+        _            => ()
+      }
     }
 }
 
