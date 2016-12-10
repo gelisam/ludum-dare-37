f7414397fc68c7161033f83e6e0cf34df1236e17
Samuel Gélineau <gelisam+github@gmail.com>
2016-12-10 13:22:35 -0500

    refactor to resources.rs

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,14 @@ extern crate time;
 use carboxyl::*;
 use carboxyl_time::*;
 use glutin_window::GlutinWindow as Window;
-use opengl_graphics::{ GlGraphics, OpenGL, Texture };
+use opengl_graphics::{ GlGraphics, OpenGL };
 use piston::event_loop::*;
 use piston::window::WindowSettings;
 
 mod font;
+mod resources;
 use font::*;
+use resources::*;
 
 
 #[derive(Clone)]
@@ -60,59 +62,6 @@ fn frp_network(raw_input_events: &Stream<RawInputEvent>) -> (Signal<Context>, Si
       );
   
   (context, state)
-}
-
-
-struct Resources {
-  big_font:      Font,
-  small_font:    Font,
-  //floor:         Texture,
-  //goal_top:      Texture,
-  //goal:          Texture,
-  //inventory_key: Texture,
-  //key:           Texture,
-  //locked:        Texture,
-  player:        Texture,
-  //sign:          Texture,
-  //spiny:         Texture,
-  //start_top:     Texture,
-  //start:         Texture,
-  //unlocked_top:  Texture,
-  //unlocked:      Texture,
-  //wall:          Texture,
-}
-
-fn load_resources() -> Resources {
-  use std::path::Path;
-  
-  Resources {
-    big_font:      Font {
-                     texture: Texture::from_path(Path::new("images/big-font.png")).unwrap(),
-                     zeroth_char: '\x00', grid_width: 25,
-                     cell_width:   20.0, cell_height:   20.0,
-                     sprite_width: 10.0, sprite_height: 20.0,
-                   },
-    small_font:    Font {
-                     texture: Texture::from_path(Path::new("images/small-font.png")).unwrap(),
-                     zeroth_char: '-', grid_width: 13,
-                     cell_width:   5.0, cell_height:   5.0,
-                     sprite_width: 5.0, sprite_height: 5.0,
-                   },
-    //floor:         Texture::from_path(Path::new("images/floor.png")).unwrap(),
-    //goal_top:      Texture::from_path(Path::new("images/goal-top.png")).unwrap(),
-    //goal:          Texture::from_path(Path::new("images/goal.png")).unwrap(),
-    //inventory_key: Texture::from_path(Path::new("images/inventory-key.png")).unwrap(),
-    //key:           Texture::from_path(Path::new("images/key.png")).unwrap(),
-    //locked:        Texture::from_path(Path::new("images/locked.png")).unwrap(),
-    player:        Texture::from_path(Path::new("images/player.png")).unwrap(),
-    //sign:          Texture::from_path(Path::new("images/sign.png")).unwrap(),
-    //spiny:         Texture::from_path(Path::new("images/spiny.png")).unwrap(),
-    //start_top:     Texture::from_path(Path::new("images/start-top.png")).unwrap(),
-    //start:         Texture::from_path(Path::new("images/start.png")).unwrap(),
-    //unlocked_top:  Texture::from_path(Path::new("images/unlocked-top.png")).unwrap(),
-    //unlocked:      Texture::from_path(Path::new("images/unlocked.png")).unwrap(),
-    //wall:          Texture::from_path(Path::new("images/wall.png")).unwrap(),
-  }
 }
 
 
---
+++ src/resources.rs
@@ -0,0 +1,56 @@
+use opengl_graphics::Texture;
+
+use font::*;
+
+
+pub struct Resources {
+  pub big_font:      Font,
+  pub small_font:    Font,
+  //pub floor:         Texture,
+  //pub goal_top:      Texture,
+  //pub goal:          Texture,
+  //pub inventory_key: Texture,
+  //pub key:           Texture,
+  //pub locked:        Texture,
+  pub player:        Texture,
+  //pub sign:          Texture,
+  //pub spiny:         Texture,
+  //pub start_top:     Texture,
+  //pub start:         Texture,
+  //pub unlocked_top:  Texture,
+  //pub unlocked:      Texture,
+  //pub wall:          Texture,
+}
+
+pub fn load_resources() -> Resources {
+  use std::path::Path;
+  
+  Resources {
+    big_font:      Font {
+                     texture: Texture::from_path(Path::new("images/big-font.png")).unwrap(),
+                     zeroth_char: '\x00', grid_width: 25,
+                     cell_width:   20.0, cell_height:   20.0,
+                     sprite_width: 10.0, sprite_height: 20.0,
+                   },
+    small_font:    Font {
+                     texture: Texture::from_path(Path::new("images/small-font.png")).unwrap(),
+                     zeroth_char: '-', grid_width: 13,
+                     cell_width:   5.0, cell_height:   5.0,
+                     sprite_width: 5.0, sprite_height: 5.0,
+                   },
+    //floor:         Texture::from_path(Path::new("images/floor.png")).unwrap(),
+    //goal_top:      Texture::from_path(Path::new("images/goal-top.png")).unwrap(),
+    //goal:          Texture::from_path(Path::new("images/goal.png")).unwrap(),
+    //inventory_key: Texture::from_path(Path::new("images/inventory-key.png")).unwrap(),
+    //key:           Texture::from_path(Path::new("images/key.png")).unwrap(),
+    //locked:        Texture::from_path(Path::new("images/locked.png")).unwrap(),
+    player:        Texture::from_path(Path::new("images/player.png")).unwrap(),
+    //sign:          Texture::from_path(Path::new("images/sign.png")).unwrap(),
+    //spiny:         Texture::from_path(Path::new("images/spiny.png")).unwrap(),
+    //start_top:     Texture::from_path(Path::new("images/start-top.png")).unwrap(),
+    //start:         Texture::from_path(Path::new("images/start.png")).unwrap(),
+    //unlocked_top:  Texture::from_path(Path::new("images/unlocked-top.png")).unwrap(),
+    //unlocked:      Texture::from_path(Path::new("images/unlocked.png")).unwrap(),
+    //wall:          Texture::from_path(Path::new("images/wall.png")).unwrap(),
+  }
+}
