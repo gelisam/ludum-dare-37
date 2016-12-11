3a12c4f80995686c8fa274aa0fac11742776d4b4
Samuel Gélineau <gelisam+github@gmail.com>
2016-12-11 13:41:54 -0500

    support chain collisions

--- src/spiny.rs
+++ src/spiny.rs
@@ -1,15 +1,43 @@
+use std::collections::HashMap;
 use graphics::math::*;
 
 use levels::*;
 use types::*;
 
 
-fn check_spiny_spiny_collision(spiny1: &MovingPos, spiny2: &MovingPos, t0: Seconds, t: Seconds) -> bool {
+fn should_bounce(
+  spiny: &MovingPos,
+  spinies_src: &HashMap<Pos, Dir>, // all the spinies, accessible by src.
+  spinies_dst: &HashMap<Pos, i8>,  // all the spinies, accessible by dst.
+  level_number: LevelNumber,
+  t0: Seconds,
+  t: Seconds
+) -> bool {
+  use levels::CellDescription::*;
+  
+  let src = spiny.pos;
+  let dst = add(src, spiny.dir);
+  
   // Spinies move in unison, that is, they are cell-aligned on the same frame and they are the same
   // fraction of the way to the next cell as every other spiny. This simplifies collisions.
   let dt = t - t0;
   
-  // Case 1: collision at a cell boundary
+  // Case 0: collision with a wall
+  //   .................
+  //   .       .       .
+  //   . >>>>> . ##### .
+  //   . >>>>> . ##### .
+  //   . >>>>> . ##### .
+  //   .       .       .
+  //   .................
+  // 
+  // This can only happen at t0, otherwise we would have bounced already.
+  match cell_at(level_number, dst) {
+    LeftDoor | RigthDoor | LockedDoor | Sign(_) | Wall => return true,
+    _                                                  => {},
+  }
+  
+  // Case 1: collision with a spiny at a cell boundary
   // 
   //   .................  .................  .................           .................
   //   .       .       .  .       .       .  .       .       .           .       .       .
@@ -19,12 +47,14 @@ fn check_spiny_spiny_collision(spiny1: &MovingPos, spiny2: &MovingPos, t0: Secon
   //   .       .       .  .       .       .  .       .       .           .       .       .
   //   .................  .................  .................           .................
   // 
-  // This can only happen at t0, otherwise spiny1 would have bounced already.
-  if add(spiny1.pos, spiny1.dir) == spiny2.pos && spiny1.dir != spiny2.dir {
-    true
+  // This can only happen at t0, otherwise we would have bounced already.
+  if let Some(spiny2_dir) = spinies_src.get(&dst) {
+    if spiny.dir != *spiny2_dir {
+      return true
+    }
   }
   
-  // Case 2: collision in the middle of a cell
+  // Case 2: collision with a spiny in the middle of a cell
   // 
   //   .........................  .................           .........................  .................
   //   .       .       .       .  .       .       .           .       .       .       .  .       .       .
@@ -41,13 +71,42 @@ fn check_spiny_spiny_collision(spiny1: &MovingPos, spiny2: &MovingPos, t0: Secon
   //                                      .........                                              .........
   // 
   // Already at t0 the collision is inevitable, but only the collision only happens after SPINY_HALF_MOVE_DURATION.
-  else if dt > SPINY_HALF_MOVE_DURATION && add(spiny1.pos, spiny1.dir) == add(spiny2.pos, spiny2.dir) {
-    true
+  if dt > SPINY_HALF_MOVE_DURATION {
+    if let Some(nb_overlapping_spinies) = spinies_dst.get(&dst) {
+      if *nb_overlapping_spinies > 1 {
+        return true
+      }
+    }
   }
   
-  else {
-    false
+  // Case 3: chain collision
+  // 
+  //   .........................  .........................
+  //   .       .       .       .  .       .       .       .
+  //   . >>>>> . >>>>> . ^^^^^ .  .     >>>>>   >>>>>     .
+  //   . >>>>> . >>>>> . ^^^^^ .  .     >>>>>   >>>>>     .
+  //   . >>>>> . >>>>> . ^^^^^ .  .     >>>>>   >>>>>     .
+  //   .       .       .       .  .       .       . ^^^^^ .
+  //   .........................  ..................^^^^^..
+  //                                              . ^^^^^ .
+  //                                              .       .
+  //                                              .       .
+  //                                              .       .
+  //                                              .       .
+  //                                              .........
+  // 
+  // If the spiny in front of us bounces onto an obstacle, we should bounce as well, otherwise we'll have a head-on
+  // collision with it in the next frame and it will bounce back towards the obstacle.
+  if let Some(spiny2_dir) = spinies_src.get(&dst) {
+    if spiny.dir == *spiny2_dir {
+      let spiny2 = MovingPos { pos: dst, dir: *spiny2_dir };
+      if should_bounce(&spiny2, spinies_src, spinies_dst, level_number, t0, t) {
+        return true;
+      }
+    }
   }
+  
+  false
 }
 
 fn bounce_spiny(spiny: &mut MovingPos, t0: Seconds, t: Seconds) {
@@ -64,29 +123,36 @@ fn bounce_spiny(spiny: &mut MovingPos, t0: Seconds, t: Seconds) {
 }
 
 pub fn update_spinies(spinies: &mut Vec<MovingPos>, level_number: LevelNumber, t0: &mut Seconds, t: Seconds) {
-  use levels::CellDescription::*;
-  
   if t >= *t0 + SPINY_MOVE_DURATION {
     *t0 = t;
     
-    for spiny in &mut spinies[..] {
-      let pos = add(spiny.pos, spiny.dir);
-      *spiny = match cell_at(level_number, add(pos, spiny.dir)) {
-        LockedDoor | Sign(_) | Wall => MovingPos { pos: pos, dir: mul_scalar(spiny.dir, -1) }, // bounce
-        _                           => MovingPos { pos: pos, dir: spiny.dir,                }, // keep moving
-      }
+    // Keep moving in the same direction, we'll handle collisions in a moment.
+    for spiny in spinies.iter_mut() {
+      *spiny = MovingPos {
+        pos: add(spiny.pos, spiny.dir),
+        dir: spiny.dir
+      };
     }
   }
   
-  let mut collisions = Vec::new();
-  for (i, spiny1) in spinies.iter().enumerate() {
-    for (j, spiny2) in spinies.iter().enumerate() {
-      if i != j && check_spiny_spiny_collision(spiny1, spiny2, *t0, t) {
-        collisions.push(i);
-      }
-    }
+  // Our spinies are on a grid, so we should be able to look up spinies by their position.
+  let mut spinies_src = HashMap::with_capacity(spinies.len());
+  for spiny in spinies.iter() {
+    spinies_src.insert(spiny.pos, spiny.dir);
   }
-  for i in collisions {
-    bounce_spiny(&mut spinies[i], *t0, t);
+  
+  // But the spinies are also moving, so their position overlaps two cells: their source and destination positions.
+  // If two spinies are about to collide head-on in the middle of a cell, they can have the same destination.
+  let mut spinies_dst = HashMap::with_capacity(spinies.len());
+  for spiny in spinies.iter() {
+    let dst = add(spiny.pos, spiny.dir);
+    *spinies_dst.entry(dst).or_insert(0) += 1;
+  }
+  
+  // We now have everything we need to determine if a spiny should bounce.
+  for spiny in spinies.iter_mut() {
+    if should_bounce(spiny, &mut spinies_src, &mut spinies_dst, level_number, *t0, t) {
+      bounce_spiny(spiny, *t0, t);
+    }
   }
 }
