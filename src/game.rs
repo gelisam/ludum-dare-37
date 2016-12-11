6ccb1345b06a90a83e88c00a5da0651aef839ed7
Samuel Gélineau <gelisam+github@gmail.com>
2016-12-10 19:14:03 -0500

    pause while a message is displayed

--- src/game.rs
+++ src/game.rs
@@ -3,12 +3,22 @@ use types::RawInputEvent::*;
 
 
 pub fn update(state: &mut State, raw_input_event: RawInputEvent) {
-  match raw_input_event {
-    TimePasses(dt) => {
-      state.time += dt;
-    },
-    Move(_) => {
-      state.message = None;
-    },
+  match state.message {
+    Some(_) =>
+      match raw_input_event {
+        TimePasses(dt) => {
+        },
+        Move(dir) => {
+          state.message = None;
+        },
+      },
+    None =>
+      match raw_input_event {
+        TimePasses(dt) => {
+          state.time += dt;
+        },
+        Move(dir) => {
+        },
+      },
   }
 }
